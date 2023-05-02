2c0ef7c3c27bd49a9069c684ff34066ebf52dacd
Gabriel Felipe Jess Meira <meira.gabriel@tecpuc.com.br>
2023-05-02 09:06:12 -0300

    ajustes

--- PAGES/templates/footers/navBar.php
+++ PAGES/templates/footers/navBar.php
@@ -1,24 +1,24 @@
 <nav>
     <button onclick="location.href= '../../CONFIG/login/logout.php' ">
         <img src="<?php echo $assets_path ?>/logOut.svg" alt="" >
-        <p style="display: none;">LogOut</p>
+        <p style="display: ;">LogOut</p>
     </button>
     <?php 
     //Verifica se é um Admin
     if ($_SESSION['TIPOUSUARIO'] == 1){?>
         <button onclick="location.href= '../admin/admin.php' ">
             <img src="<?php echo $assets_path ?>/admin.svg" alt="" >
-            <p style="display: none;">Painel do administrador</p>
+            <p style="display: ;">Painel do administrador</p>
         </button>   
     <?php 
     }?>                
     <button onclick="location.href ='../atividades_ao_ar_livre/insert/insert_atividade.php';">
         <img src="<?php echo $assets_path ?>/buttonNewPubli.svg" alt="">
-        <p style="display: none;">Publicar</p>
+        <p style="display: ;">Publicar</p>
     </button>
     <button>
         <img src="<?php echo $assets_path ?>/buttonPerfil.svg" alt="">
-        <p style="display: none;">Perfil</p>
+        <p style="display: ;">Perfil</p>
     </button>
 
 </nav>
