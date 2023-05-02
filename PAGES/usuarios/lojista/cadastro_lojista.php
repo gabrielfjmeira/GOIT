8f18d04f053665349f682067d4dfbc738de27e20
Gabriel Felipe Jess Meira <meira.gabriel@tecpuc.com.br>
2023-05-02 10:14:10 -0300

    Cadastro de Usuarios revisados

--- PAGES/usuarios/lojista/cadastro_lojista.php
+++ PAGES/usuarios/lojista/cadastro_lojista.php
@@ -36,15 +36,16 @@
         <div class="input-wrapper">
             <label>Razão Social: </label>
             <input type="text" id="txtRazaoSocial" name="txtRazaoSocial" placeholder="Razão Social" class="input" 
-            pattern="^.{8,}$" 
-            title="Razão Social deve possuir no mínimo 8 caracteres e no máximo 100 caracteres!" required/>
+            pattern="^[A-Z][a-z]+[\s]*(([A-Z]||[a-z])[a-z]{1,}[\s]*){0,}$" 
+            title="Razão Social só deve conter letras e deve possuir no mínimo 3 caracteres e no máximo 100 caracteres" required/>
+            <small id="errorRazaoSocial" style="color: #DB5A5A; margin-left: 0.6rem; margin-top: 0.4rem;"></small>
         </div>
 
         <div class="input-wrapper">    
             <label>Fantasia: </label>
             <input type="text" id="txtFantasia" name="txtFantasia" placeholder="Fantasia" class="input" 
-            pattern="^.{4,30}$" 
-            title="Fantasia deve possuir no mínimo 4 caracteres e no máximo 30 caracteres!" required/>
+            pattern="^[A-z]\w{3,29}$" 
+            title="Fantasia deve começar com uma letra e não pode conter símbolos, deve possuir no mínimo 4 caracteres e no máximo 30 caracteres!" required/>
             <small id="errorFantasia" style="color: #DB5A5A; margin-left: 0.6rem; margin-top: 0.4rem;"></small>
         </div>
 
