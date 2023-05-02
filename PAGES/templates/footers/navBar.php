ca750cfe56615415f248c5ebe3c9ca9254499132
Sidnei Henrique <henrisrs@hotmail.com>
2023-05-02 00:08:17 -0300

    Homepage parcialmente responsiva

--- PAGES/templates/footers/navBar.php
+++ PAGES/templates/footers/navBar.php
@@ -1,11 +1,24 @@
-<nav class="flex">
-    <button><img src="<?php echo $assets_path ?>/logOut.svg" alt="" onclick="location.href= '../../CONFIG/login/logout.php' "></button>
+<nav>
+    <button onclick="location.href= '../../CONFIG/login/logout.php' ">
+        <img src="<?php echo $assets_path ?>/logOut.svg" alt="" >
+        <p style="display: none;">LogOut</p>
+    </button>
     <?php 
     //Verifica se é um Admin
     if ($_SESSION['TIPOUSUARIO'] == 1){?>
-        <button><img src="<?php echo $assets_path ?>/admin.svg" alt="" onclick="location.href= '../admin/admin.php' "></button>   
+        <button onclick="location.href= '../admin/admin.php' ">
+            <img src="<?php echo $assets_path ?>/admin.svg" alt="" >
+            <p style="display: none;">Painel do administrador</p>
+        </button>   
     <?php 
     }?>                
-    <button><img src="<?php echo $assets_path ?>/buttonNewPubli.svg" alt="" onclick="location.href ='../atividades_ao_ar_livre/insert/insert_atividade.php';"></button>
-    <button><img src="<?php echo $assets_path ?>/buttonPerfil.svg" alt=""></button>
+    <button onclick="location.href ='../atividades_ao_ar_livre/insert/insert_atividade.php';">
+        <img src="<?php echo $assets_path ?>/buttonNewPubli.svg" alt="">
+        <p style="display: none;">Publicar</p>
+    </button>
+    <button>
+        <img src="<?php echo $assets_path ?>/buttonPerfil.svg" alt="">
+        <p style="display: none;">Perfil</p>
+    </button>
+
 </nav>
